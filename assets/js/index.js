c28f387c0479c4f7b6581be1da1493dcaeb20cc1
pamearteaga <pe_arteaga@hotmail.com>
2018-01-24 20:26:51 -0300

    función

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,18 +2,28 @@ window.onload = function() {
 
   console.log("listo");
   
-  $('body').cardify({
-    
+  $("img").wrap("<figure></figure>");
+  //$('body').cardify({});
+
+  $("img").mouseover(function(){
+    var caption = $(this).attr("alt");
+    $(this).css("opacity", "0.3");
+    $(this).before('<figcaption>' + caption + '</figcaption>');
+    $("figcaption").css({"font-family": "sans-serif", "font-size": "2em", "font-weight": "bold", "position": "relative", "top": "1em", "left": "1em"});
+  })
+
+  $("img").mouseout(function(){
+    $(this).css("opacity", "1");
+    $("figcaption").remove();
   });
+
 
 };//onload
 
-function
+function cardify() {
 
 
-$('img').css("opacity","0.3");
-
-
+};
 
 
 
