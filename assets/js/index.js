e87bf3a1705b0caf0d651f98cd4c694657ead031
pamearteaga <pe_arteaga@hotmail.com>
2018-01-26 13:07:44 -0300

    detalles función

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,9 +8,9 @@ window.onload = function() {
 
   $("img").mouseover(function(){
     const caption = $(this).attr("alt");
-    $(this).css("opacity", "0.3");
+    $(this).css("opacity", "0.2");
     $(this).after('<figcaption>' + caption + '</figcaption>');
-    $("figcaption").css({"width": "200px","height": "auto", "color": "white", "font-family": "sans-serif", "font-size": "2em", "font-weight": "bold", "position": "absolute", "margin": "-15% 0 0 2%", "padding": "0", "display": "inline-block", "vertical-align": "top"});
+    $("figcaption").css({"width": "200px","height": "auto", "color": "white", "font-family": "sans-serif", "font-size": "2em", "font-weight": "bold", "position": "absolute", "margin": "-10% 0 0 2%", "padding": "0", "display": "inline-block", "cursor": "pointer"});
     $(this).wrap("<figure></figure>");
     $("figure").css({"width": "auto", "height": "100%", "margin": "0", "padding": "0", "background": "linear-gradient(to bottom right, rgb(52,247,143), rgb(245,95,240))"});
   })
