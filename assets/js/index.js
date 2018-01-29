19d7b3262d16b53e0ce816596c5e982fa0459ad0
pamearteaga <pe_arteaga@hotmail.com>
2018-01-29 17:49:23 -0300

    función modo plugin

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,36 +1,25 @@
+const $ = require('jquery');
 
-window.onload = function() {
-
-  const test = $("body").find("img");
-
-  //$('body').cardify({});
-  $("img").mouseover(function(){
-    const caption = $(this).attr("alt");
-    $(this).css("opacity", "0.2");
-    $(this).after('<figcaption>' + caption + '</figcaption>');
-    $("figcaption").css({"width": "200px","height": "auto", "color": "white", "font-family": "sans-serif", "font-size": "2em", "font-weight": "bold", "position": "absolute", "margin": "-10% 0 0 2%", "padding": "0", "display": "inline-block", "cursor": "pointer"});
-    $(this).wrap("<figure></figure>");
-    $("figure").css({"width": "auto", "height": "100%", "margin": "0", "padding": "0", "background": "linear-gradient(to bottom right, rgb(52,247,143), rgb(245,95,240))"});
-  });
-
-  $("img").mouseout(function(){
-    $(this).css("opacity", "1");
-    $(this).unwrap();
-    $("figcaption").remove();
-  });
-};//onload
-
-function cardify() {
-
-};
-
-
-/*
-//Formato plugin
-$(document).ready(() => {
-  $('.contain').cardify(); // sirve para cualquier clase de container.
+$(document).ready(function() {
+  $('body').cardify({});
 });
-(function($) {
 
-})($);
-*/
+(function($) {
+  $.fn.cardify = function() {
+    return this.each(function() {
+      $('img').mouseover(function() {
+        const caption = $(this).attr('alt');
+        $(this).css("opacity", "0.2");
+        $(this).after('<figcaption>' + caption + '</figcaption>');
+        $('figcaption').css({"width": "200px", "height": "auto", "color": "white", "font-family": "sans-serif", "font-size": "2em", "font-weight": "bold", "position": "absolute", "margin": "-10% 0 0 2%", "padding": "0", "display": "inline-block", "cursor": "pointer"});
+        $(this).wrap('<figure></figure>');
+        $('figure').css({"width": "auto", "height": "100%", "margin": "0", "padding": "0", "background": "linear-gradient(to bottom right, rgb(52,247,143), rgb(245,95,240))"});
+      });
+      $('img').mouseout(function() {
+        $(this).css("opacity", "1");
+        $(this).unwrap();
+        $('figcaption').remove();
+      });
+    });
+  };
+})(jQuery);
